deaa068293fcb979cd938ad72a359e165e99f29b
Darren Reid <darren@reidmail.org>
2021-11-30 16:24:21 +1100

    Initial commit

---
+++ WebNet5.ServiceInterface/MyServices.cs
@@ -0,0 +1,14 @@
+using System;
+using ServiceStack;
+using WebNet5.ServiceModel;
+
+namespace WebNet5.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
